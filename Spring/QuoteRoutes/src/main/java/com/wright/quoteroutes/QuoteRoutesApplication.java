792c2f1d75d5ad2d8ea5cce1444baf1884a90eb0
Patrick Wright <patrickdane@me.com>
2022-02-11 12:49:15 -0500

    algos

---
+++ Spring/QuoteRoutes/src/main/java/com/wright/quoteroutes/QuoteRoutesApplication.java
@@ -0,0 +1,13 @@
+package com.wright.quoteroutes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuoteRoutesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuoteRoutesApplication.class, args);
+	}
+
+}
---
+++ Spring/QuoteRoutes/src/test/java/com/wright/quoteroutes/QuoteRoutesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wright.quoteroutes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuoteRoutesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
